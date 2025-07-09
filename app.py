9bd29c0bc7ec50ba1755f9e0ee951d3daa0e0e05
harshbhatnagar-dotcom <harsh210806@gmail.com>
2025-07-09 13:01:18 +0530

    version 2

--- app.py
+++ app.py
@@ -7,11 +7,21 @@ app=Flask(__name__)
 model=joblib.load("model.pkl")
 mlb=joblib.load("label.pkl")
 
-csv_path = os.path.join(os.path.dirname(__file__), "data", "Disease_Test_Recommendations.csv")
-
-# Load the test mapping
-test_df = pd.read_csv(csv_path)
+base_dir = os.path.dirname(__file__)
+# Load Recommended Tests
+tests_path = os.path.join(base_dir, "data", "Disease_Diagnostic_Tests.csv")
+test_df = pd.read_csv(tests_path)
 test_map = dict(zip(test_df["Disease"], test_df["Recommended Tests"]))
+
+# Load Precautions
+precautions_path = os.path.join(base_dir, "data", "Disease_Precautions.csv")
+precautions_df = pd.read_csv(precautions_path)
+precautions_map = dict(zip(precautions_df["Disease"], precautions_df["Precautions"]))
+
+# Load Causes
+causes_path = os.path.join(base_dir, "data", "Disease_Causes.csv")
+causes_df = pd.read_csv(causes_path)
+causes_map = dict(zip(causes_df["Disease"], causes_df["Causes"]))
 
 @app.route("/")
 def home():
@@ -27,13 +37,22 @@ def predict():
 
     
     test_info = {}
+    precaution_info = {}
+    cause_info = {}
 
     for disease in predictions:
-        if disease in test_map:
-            test_info[disease] = test_map[disease]
-        else:
-            test_info[disease] = "No test info available"
-    return render_template("predict.html", symptoms=symptoms, predictions=predictions,test_info=test_info)
+        test_info[disease] = test_map.get(disease, "No test info available")
+        precaution_info[disease] = precautions_map.get(disease, "No precaution info available")
+        cause_info[disease] = causes_map.get(disease, "No cause info available")
+
+    return render_template(
+        "predict.html",
+        symptoms=symptoms,
+        predictions=predictions,
+        test_info=test_info,
+        precaution_info=precaution_info,
+        cause_info=cause_info
+    )
 
 
 if __name__ =="__main__":
